008279af49314f7b33dcef5c59282d1b5bd68e94
velosobr <linoc.veloso@gmail.com>
2019-08-08 23:57:24 -0300

    Implementação da pasta de estrutura de dados

---
+++ ine5609 - data structure/Aula 1/ArrayPascalInt.java
@@ -0,0 +1,10 @@
+Class arrayPascalInt{
+  int[] dados;
+  int li, ls;
+
+  arrayPascalInt(int limInf, int limSup){
+    dados = new int[limSup - limInf +1];
+    li = limInf;
+    ls = limSup;
+  }
+}
---
+++ ine5609 - data structure/Aula 1/usoApi.java
@@ -0,0 +1,5 @@
+ArrayPascalInt meuA = new ArrayPascalInt(1,5);
+
+meuA.atribuir(2,42);//pos e valor
+
+int x = meuA.acessar(3);//pos
