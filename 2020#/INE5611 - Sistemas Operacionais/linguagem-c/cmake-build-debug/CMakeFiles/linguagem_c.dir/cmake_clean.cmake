d70041d7171e482dd008ee3a59be02a2fde551fd
velosobr <linoc.veloso@gmail.com>
2020-09-17 22:09:01 -0300

    linguagem C

---
+++ 2020#/INE5611 - Sistemas Operacionais/linguagem-c/cmake-build-debug/CMakeFiles/linguagem_c.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linguagem_c.dir/main.c.o"
+  "linguagem_c"
+  "linguagem_c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/linguagem_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
