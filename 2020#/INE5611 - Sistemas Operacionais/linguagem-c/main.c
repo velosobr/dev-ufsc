50ec2a452e07ae506ebcea4db1802849ef73e8b3
Lino Veloso <linoc.veloso@gmail.com>
2020-09-19 22:58:55 -0300

    Update main.c

--- 2020#/INE5611 - Sistemas Operacionais/linguagem-c/main.c
+++ 2020#/INE5611 - Sistemas Operacionais/linguagem-c/main.c
@@ -2,5 +2,32 @@
 
 int main() {
     printf("Hello, World!\n");
+  
+    /* Variables_and_Types */
+  int a = 3;
+  float b = 4.5;
+  double c = 5.25;
+  float sum = a + b + c;
+
+
+  printf("The sum of a, b, and c is %f.", sum);
+    
+
+    /* Array */
+    int grades[3];
+    int average;
+
+  grades[0] = 80;
+ grades[1] = 85;
+  grades[2] = 90;
+
+  average = (grades[0] + grades[1] + grades[2]) / 3;
+  printf("The average of the 3 grades is: %d", average);
+    
+    /* Multidimensional_Arrays */
+
+
     return 0;
+    
+    
 }
