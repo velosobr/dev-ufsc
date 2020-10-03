68543901600402d893fad9a2504bf44aea9b3f9a
velosobr <linoc.veloso@gmail.com>
2020-10-02 21:32:31 -0300

    Aula thread SO

---
+++ 2020#/INE5611 - Sistemas Operacionais/linguagem-c/Threads/cmake-build-debug/CMakeFiles/Threads.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Threads.dir/main.c.o"
+  "Threads"
+  "Threads.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Threads.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
