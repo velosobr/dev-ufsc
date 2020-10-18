a78f66fd1d649cc1702296735d4af9659649439b
Lino Veloso <linoc.veloso@gmail.com>
2020-10-17 23:44:34 -0300

    Update main.c

--- 2020#/INE5611 - Sistemas Operacionais/linguagemc/Ponteiros/main.c
+++ 2020#/INE5611 - Sistemas Operacionais/linguagemc/Ponteiros/main.c
@@ -38,5 +38,18 @@ int main() {
     max_min(&max, &min);
 
 
+
+  int n = 10;
+
+  int * pointer_to_n = &n;
+
+  *pointer_to_n += 1;
+
+  /* testing code */
+  if (pointer_to_n != &n) return 1;
+  if (*pointer_to_n != 11) return 1;
+
+  printf("Done!\n");
+
     return 0;
 }
