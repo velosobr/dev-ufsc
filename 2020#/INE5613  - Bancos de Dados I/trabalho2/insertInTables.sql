9ea007a02a9212efb7ee9f2a18a267d17810928d
velosobr <linoc.veloso@gmail.com>
2020-11-07 23:54:05 -0300

    insert dados para teste

---
+++ 2020#/INE5613  - Bancos de Dados I/trabalho2/insertInTables.sql
@@ -0,0 +1,51 @@
+
+insert into Funcionarios(cpf, nome, salario, turno, funcao)
+VALUES (77472670022, 'Mirella Alícia Pires', 9879.00, 'M', 'seguranca'),
+       (06342194130, 'Marcos Diogo Fernandes', 954.00, 'N', 'seguranca'),
+       (69555554560, 'Aline Emilly Peixoto', 1654.00, 'T', 'seguranca'),
+       (53354702834, 'Jaqueline Maria Nogueira', 1354.00, 'M', 'seguranca'),
+       (40844516309, 'Nathan Hugo da Conceição', 9856.00, 'T', 'faxineiro'),
+       (26489952777, 'Otávio Theo Lima', 123.00, 'T', 'faxineiro'),
+       (42952571503, 'Benedito Matheus Baptista', 875.00, 'N', 'faxineiro'),
+       (15909598848, 'Marcela Evelyn Juliana das Neves', 798.00, 'M', 'faxineiro');
+-- Artistas plasticos
+insert into Autores(codigo, nome, nacionalidade)
+VALUES (1, 'Tomie Ohtake', 'Japonesa'),
+       (2, 'Hideko Honma', 'Japonesa'),
+       (3, 'Adriana Varejão', 'Japonesa'),
+       (4, 'Victor Brecheret', 'Italiana'),
+       (5, 'Vik Muniz', 'Brasileira'),
+       (6, 'Beatriz Milhazes', 'Brasileira'),
+       (7, 'Hélio Oiticica', 'Brasileira');
+
+
+insert into Obras(codigo, titulo, ano, autor, salao)
+VALUES (1, 'corpo feminino desnudo', 1956, 8, 1),
+       (2, 'Monte das Oliveiras', 1796, 9, 1),
+       (3, 'Flagelacao', 1796, 9, 1),
+       (4, 'Cristo é Aprisionado', 1796, 9, 1),
+       (5, 'Profeta Jeremias', 1800, 9, 1),
+       (6, 'O Corpo da Janela', 2001, 10, 1),
+       (7, 'Kuantico: A Odisséia Continua Navedante', 1994, 10, 1),
+       (8, 'Vênus', 2020, 11, 1),
+       (9, 'Halia', 2019, 11, 1),
+       (10, 'Halia', 2019, 11, 1),
+       (11, 'Halia', 2019, 11, 1),
+       (12, 'Halia', 2019, 11, 1),
+       (13, 'Halia', 2019, 11, 1),
+       (14, 'Halia', 2019, 11, 1),
+       (15, 'Halia', 2019, 11, 1),
+       (16, 'Halia', 2019, 11, 1),
+       (17, 'Halia', 2019, 11, 1),
+       (18, 'Halia', 2019, 11, 1),
+
+-- Artistas escultures https://www.ebiografia.com/escultores_famosos_suas_obras_incriveis/
+insert into Autores(codigo, nome, nacionalidade)
+VALUES (8, 'Abelardo da Hora', 'Brasileira'),
+       (9, 'Aleijadinho', 'Brasileira'),
+       (10, 'Ernesto Neto', 'Brasileira'),
+       (11, 'Francisco Brennand', 'Brasileira');
+
+
+insert into Esculturas(codigo, altura, peso, material)
+VALUES (1 A)
